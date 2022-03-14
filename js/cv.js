c8c55d73a607be95b030a0082e00e04a29f2127b
Gerson Cardoso Filho <gersoncfilho@mac.com>
2022-03-14 17:29:09 +0000

    finished charts

--- js/cv.js
+++ js/cv.js
@@ -4,20 +4,56 @@ $(document).ready(function(){
     google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
 
-        let totvsData = google.visualization.arrayToDataTable([
-            ['Skill', ''],
-            ['C#', 10],
-            ['SQL', 10],
-            ['Postman',  10],
-            ['Jira', 2],
-        ])
-
         let natixisData = google.visualization.arrayToDataTable([
             ['Skill', ''],
             ['Webforms', 7],
             ['MVC',      7],
             ['SQL',  5],
             ['TFS', 2],
+        ]);
+
+        let totvsData = google.visualization.arrayToDataTable([
+            ['Skill', ''],
+            ['C#', 9],
+            ['SQL Server', 3],
+            ['Postman',  3],
+            ['Jira', 2],
+            ['DDD',4]
+        ]);
+
+        let tataData = google.visualization.arrayToDataTable([
+            ['Skill', ''],
+            ['C#', 10],
+            ['SQL', 10],
+            ['Azure',  10],
+            ['TFS', 2],
+            ['Postman', 2]
+        ]);
+
+        let accentureData = google.visualization.arrayToDataTable([
+            ['Skill', ''],
+            ['Oracle', 10],
+            ['Pega Systems', 40],
+        ]);
+
+        let wiproData = google.visualization.arrayToDataTable([
+            ['Skill', ''],
+            ['Oracle', 10],
+            ['Postman', 20],
+            ['C#', 40],
+        ]);
+
+        let ccaaData = google.visualization.arrayToDataTable([
+            ['Skill', ''],
+            ['SQL Server', 10],
+            ['Asp Net MVC', 20],
+            ['C#', 40],
+        ]);
+
+        let golData = google.visualization.arrayToDataTable([
+            ['Skill', ''],
+            ['SQL Lite', 3],
+            ['Java', 20],
         ]);
 
         let options = {
@@ -30,7 +66,20 @@ $(document).ready(function(){
 
         let chartTotvs = new google.visualization.PieChart(document.getElementById('piechart_3d_totvs'));
         chartTotvs.draw(totvsData, options);
+
+        let chartTata = new google.visualization.PieChart(document.getElementById('piechart_3d_tata'));
+        chartTata.draw(tataData, options);
+
+        let chartAccenture = new google.visualization.PieChart(document.getElementById('piechart_3d_accenture'));
+        chartAccenture.draw(accentureData, options);
+
+        let chartWipro = new google.visualization.PieChart(document.getElementById('piechart_3d_wipro'));
+        chartWipro.draw(wiproData, options);
+
+        let chartCCAA = new google.visualization.PieChart(document.getElementById('piechart_3d_ccaa'));
+        chartCCAA.draw(ccaaData, options);
+
+        let chartGol = new google.visualization.PieChart(document.getElementById('piechart_3d_gol'));
+        chartGol.draw(golData, options);
     }
-
-
 });
